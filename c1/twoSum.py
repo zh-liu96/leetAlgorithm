8bbde2f5faaac80b3de3ee4a13c9046cf8c6b78f
zh-liu96 <liuzhenghu1993@163.com>
2017-07-10 16:38:58 +0800

    Update twoSum.py

--- c1/twoSum.py
+++ c1/twoSum.py
@@ -4,7 +4,8 @@ Created on Mon Jul 10 16:02:24 2017
 
 @author: 34375
 
- Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ Given an array of integers, return indices of the two numbers such 
+ that they add up to a specific target.
  Given nums = [2, 7, 11, 15], target = 9,
  Because nums[0] + nums[1] = 2 + 7 = 9,
  return [0, 1].
